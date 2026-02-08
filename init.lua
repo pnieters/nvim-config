7ff4a541c292ba3fb684e39323838a5d2bbc7f71
Pascal Nieters <pascal.nieters@uni-osnabrueck.de>
2026-02-08 15:30:44 +0100

    minor keymap fixes

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-vim.hl = vim.highlight
-
 require("config.lazy")
 require("everforest").load()
 
@@ -7,10 +5,10 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", ":.lua <CR>")
 vim.keymap.set("v", "<leader>x", ":lua <CR>")
 
--- i forgot what these were for 
--- vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
--- vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
--- vim.keymap.set("n", "<M-q>", "<cmd>cclose<CR>")
+-- navigate Quickfix list with mod j k , close with mod q
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<M-q>", "<cmd>cclose<CR>")
 
 vim.keymap.set("n", "<leader>-", "<cmd>Oil<CR>")
 
@@ -18,7 +16,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highligh when yanking copying text',
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end
 })
 
@@ -34,11 +32,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client)
-        if client == nil then
-            return
-        end
-        if client.name == 'ruff' then
-            -- Disable what Ruff should not do
+        if client and client.name == 'ruff' then
             client.server_capabilities.hoverProvider = false
         end
     end,
@@ -51,16 +45,4 @@ vim.keymap.set("n", "<space>st", function()
     vim.cmd.term()
     vim.cmd.wincmd("J")
     vim.api.nvim_win_set_height(0, 10)
-
-    -- get channel of term to be able to send cmds to it. likely replaced by floatterm.
-    -- job_id = vim.bo.channel
 end)
-
--- send anything to the terminal -- extend to send code to REPL?
--- vim.keymap.set("n", "<space>example", function()
---     vim.fn.chansend(job_id, { "echo 'hi \n'" })
--- end)
--- send anything to the terminal -- extend to send code to REPL?
--- vim.keymap.set("n", "<space>example", function()
---     vim.fn.chansend(job_id, { "echo 'hi \n'" })
--- end)
